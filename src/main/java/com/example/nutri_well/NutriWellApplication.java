95b84966f2c0639ace6338076e676d8ea7fc8219
6worry <dudfhr217@naver.com>
2024-06-12 13:37:32 +0900

    add spring initializr

---
+++ src/main/java/com/example/nutri_well/NutriWellApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutri_well;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriWellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriWellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nutri_well/NutriWellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutri_well;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriWellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
